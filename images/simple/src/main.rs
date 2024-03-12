60caca30874079f6cf1996193ab43e4c0fd7fee5
austbot <me@austbot.com>
2024-03-11 22:32:10 -0500

    update readme

--- images/simple/src/main.rs
+++ images/simple/src/main.rs
@@ -1,20 +1,32 @@
 
-use std::str::from_utf8;
 use gjson::Kind;
-use risc0_zkvm::guest::env;
-
+use risc0_zkvm::{guest::{env, sha::Impl},sha::{Digest, Sha256}};
+//Its a good Idea to make a Type for your inputs
+type Input = (String, String, String);
+//The output type must be a tuple with the first element being the digest of the inputs
+type Output = (Digest, bool);
 fn main() {
-    let mut data = Vec::new();
-    env::read_slice(&mut data);
-    let st = from_utf8(&data).unwrap();
-    let valid = gjson::valid(st);
+    // The Bonsol Host will send your inputs in a tuple in the order they were declared on chain
+    let (
+        publici1,  //json
+        publici2,  //path
+        privatei2, //value
+    ): Input = env::read();
+    let valid = gjson::valid(&publici1);
+    let mut res = false;
     if valid {
-        let val = gjson::get(st, "attestation");
-        if val.kind() == Kind::String {
-            env::commit(&1);
-            return;
+        let val = gjson::get(&publici1, &publici2);
+        if val.kind() == Kind::String && val.str() == privatei2 {
+            res = true;
         }
     }
-    env::commit(&0);
-    env::commit(&0);
+    let digest = Impl::hash_bytes(
+        &[
+            publici1.as_bytes(),
+            publici2.as_bytes(),
+            privatei2.as_bytes(),
+        ].concat(),
+    );
+    //Type argument here is optional but hepls enforce the type of the output
+    env::commit::<Output>(&(*digest, res));
 }
