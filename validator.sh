1480fba2e3a5ec507cc40b2695143db6c78cc655
austbot <me@austbot.com>
2024-04-06 11:33:41 -0500

    update libs to support all rust proving

--- validator.sh
+++ validator.sh
@@ -1,3 +1,6 @@
 #!/bin/zsh
 cargo --version
-cargo build-sbf && solana-test-validator --rpc-pubsub-enable-block-subscription --bpf-program BoNSrwTtTM4PRkbbPvehk1XzHC65cKfdNSod9FyTejRn target/deploy/anagram_bonsol_channel.so  -r
+cd onchain
+cargo build-sbf
+cd ..
+solana-test-validator --rpc-pubsub-enable-block-subscription --bpf-program BoNSrwTtTM4PRkbbPvehk1XzHC65cKfdNSod9FyTejRn onchain/target/deploy/anagram_bonsol_channel.so  -r
