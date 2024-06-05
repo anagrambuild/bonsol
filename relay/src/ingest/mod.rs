7934af03750d24e69777e4a48d8194e4b65831ed
Ben Orgera <benorg@me.com>
2024-06-05 21:57:50 +0000

    Geyser client subscription and tx filtering

--- relay/src/ingest/mod.rs
+++ relay/src/ingest/mod.rs
@@ -1,4 +1,5 @@
 use tokio::sync::mpsc::UnboundedSender;
+use std::convert::TryInto;
 
 use {
     anyhow::Result,
@@ -11,13 +12,24 @@ use {
 use {
     solana_sdk::pubkey::Pubkey,
     solana_transaction_status::{
-        EncodedTransactionWithStatusMeta, UiInnerInstructions, UiInstruction, UiTransactionEncoding,
+        EncodedTransactionWithStatusMeta, UiInnerInstructions, UiInstruction, UiTransactionEncoding
     },
     std::{
         error::Error,
         fmt::{self, Display, Formatter},
     },
     tokio::{sync::mpsc::UnboundedReceiver, task::JoinHandle},
+};
+
+// use solana_sdk::{pubkey::Pubkey, transaction::VersionedTransaction};
+
+use {
+    std::{collections::HashMap, time::Duration},
+    yellowstone_grpc_client::GeyserGrpcClient,
+    yellowstone_grpc_proto::prelude::{
+        subscribe_update::UpdateOneof, CommitmentLevel, SubscribeRequest,
+        SubscribeRequestFilterBlocks
+    },
 };
 
 use crate::types::BonsolInstruction;
@@ -204,9 +216,113 @@ impl Ingester for RpcIngester {
     }
 }
 
+fn to_fixed<T, const N: usize>(v: Vec<T>) -> [T; N] {
+    v.try_into()
+        .unwrap_or_else(|v: Vec<T>| panic!("Expected a Vec of length {} but it was {}", N, v.len()))
+}
+
+// todo find a way to consume without clone
+fn filter_txs2(
+    program: &Pubkey,
+    last_known_block: u64,
+    tx: yellowstone_grpc_proto::geyser::SubscribeUpdateTransactionInfo,
+) -> Vec<BonsolInstruction> {
+    let program_bytes = program.to_bytes();
+    let mut res = vec![];
+    if let Some(dtx) = tx.transaction {
+        let msg = dtx.message.unwrap();
+        let scc = msg.account_keys;
+        if let Some(meta) = tx.meta {
+            if meta.err.is_some() {
+                return res;
+            }
+            for ix in msg.instructions.iter() {
+
+                if scc[ix.program_id_index as usize] == program_bytes {
+                    res.push(BonsolInstruction {
+                        cpi: false,
+                        accounts: ix.accounts.iter().map(|a| Pubkey::from(to_fixed::<u8, 32>(scc[*a as usize].clone()))).collect(),
+                        data: ix.data.clone(),
+                        last_known_block,
+                    });
+                }
+            }
+            let inner_ix_groups: Vec<yellowstone_grpc_proto::prelude::InnerInstructions> = meta.inner_instructions;
+            for group in inner_ix_groups {
+                for ix in group.instructions {
+                    if scc[ix.program_id_index as usize] == program_bytes {
+                        let data = bs58::decode(&ix.data).into_vec();
+                        if let Ok(data) = data {
+                            res.push(BonsolInstruction {
+                                cpi: false,
+                                accounts: ix
+                                    .accounts
+                                    .iter()
+                                    .map(|a| Pubkey::from(to_fixed::<u8, 32>(scc[*a as usize].clone())))
+                                    .collect(),
+                                data,
+                                last_known_block,
+                            });
+                        } else {
+                            println!(
+                                "Failed to decode bs58 data for bonsol instruction"
+                            );
+                        }
+                    }
+                }
+            }
+        }
+    }
+    res
+}
+
 impl Ingester for GrpcIngester {
     fn start(&mut self, program: Pubkey) -> Result<TxChannel> {
-        todo!()
+        let (txchan, rx) = tokio::sync::mpsc::unbounded_channel();
+        let grpc_url = self.grpc_url.clone();
+        self.op_handle = Some(tokio::spawn(async move {
+            loop {
+                let mut client = GeyserGrpcClient::build_from_shared(grpc_url.clone())
+                .ok()
+                .unwrap()
+                .connect_timeout(Duration::from_secs(10))
+                .timeout(Duration::from_secs(10))
+                .connect()
+                .await
+                .ok()
+                .unwrap();
+
+                let subscription = client.subscribe_with_request(Some(SubscribeRequest {
+                    blocks: HashMap::from_iter(vec![ ("blocks".to_string(), SubscribeRequestFilterBlocks{
+                        account_include: vec![ program.to_string() ],
+                        include_transactions: Some(true),
+                        include_accounts: Some(false),
+                        include_entries: Some(false)
+                    }) ]),
+                    commitment: Some(CommitmentLevel::Confirmed.into()),
+                    ..Default::default()
+                })).await;
+                let (grpc_tx, mut grpc_rx) = subscription.unwrap();
+                eprintln!("Subscribed to {}", grpc_url);
+
+                while let Some(message) = grpc_rx.next().await {
+                    match message {
+                        Ok(msg) => match msg.update_oneof {
+                            Some(UpdateOneof::Block(b)) => {
+                                let height = b.block_height.unwrap().block_height;
+                                let ix = b.transactions.iter().map(|tx| {
+                                    filter_txs2(&program, height, tx.clone())
+                                }).flatten().collect::<Vec<BonsolInstruction>>();
+                                txchan.send(ix).unwrap();
+                            },
+                            _ => unreachable!(),
+                        },
+                        _ => unreachable!(),
+                    }
+                }
+            }
+        }));
+        Ok(rx)
     }
 
     fn stop(&mut self) -> Result<()> {
