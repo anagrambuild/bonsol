51de5b8edf740ec60365eeb2ba750e8e28123a03
Ben Orgera <benorg@me.com>
2024-06-05 19:42:03 +0000

    Protos building with experimental proto3 optional feature

--- relay/build.rs
+++ relay/build.rs
@@ -1,7 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut config = prost_build::Config::new();
-    config.protoc_arg("--experimental_allow_proto3_optional");
+    tonic_build::configure().protoc_arg("--experimental_allow_proto3_optional").compile(&["proto/geyser.proto"], &["proto"]).unwrap();
 
-    tonic_build::compile_protos("proto/geyser.proto")?;
     Ok(())
 }
