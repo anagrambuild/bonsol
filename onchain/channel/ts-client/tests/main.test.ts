444ac8ff2f0d7c353a0f506bcd7630fd074247bb
austbot <me@austbot.com>
2024-06-14 16:46:23 -0500

    update tests

--- onchain/channel/ts-client/tests/main.test.ts
+++ onchain/channel/ts-client/tests/main.test.ts
@@ -1,16 +1,19 @@
 import { Cluster, LuzidSdk } from '@luzid/sdk'
+import fs from 'fs/promises';
 import {
-  appendTransactionInstruction,
-  createTransaction,
   signTransaction,
   lamports,
-  setTransactionFeePayer,
   getBase64EncodedWireTransaction,
-  setTransactionLifetimeUsingBlockhash,
   createSolanaRpcSubscriptions,
-  createDefaultTransactionSender,
-  createSolanaRpc, generateKeyPair, createDefaultRpcTransport, address, getAddressFromPublicKey, createDefaultAirdropRequester, signature, getSignatureFromTransaction
+  createKeyPairFromBytes,
+  createSolanaRpc, generateKeyPair, createDefaultRpcTransport, address, getAddressFromPublicKey, signature, getSignatureFromTransaction,
+  createTransactionMessage,
+  setTransactionMessageFeePayer,
+  setTransactionMessageLifetimeUsingBlockhash,
+  appendTransactionMessageInstruction,
+  compileTransaction
 } from '@solana/web3.js';
+
 
 import { pipe } from '@solana/functional';
 import { createDefaultRpcSubscriptionsTransport } from '@solana/web3.js';
@@ -20,18 +23,19 @@ import { randomBytes } from '@noble/hashes/utils';
 
 describe('BonsolProgram', () => {
   const SIMPLE_IMAGE_ID = "111fd1d8f623c845a1d5ac7a6625159b6a0e935561de3e2bab94d9b8abfbdccc";
+  const api = createSolanaRpc(process.env.RPC_ENDPOINT || Cluster.Development.apiUrl);
+  const sub = createSolanaRpcSubscriptions((process.env.RPC_ENDPOINT || Cluster.Development.apiUrl).replace('http', 'ws').replace("8899", "8900"))
 
-  const tsp = createDefaultRpcTransport({ url: Cluster.Development.apiUrl })
-  const ssp = createDefaultRpcSubscriptionsTransport({
-    url: Cluster.Development.apiUrl.replace('http', 'ws').replace("8899", "8900")
-  })
-  const api = createSolanaRpc({ transport: tsp });
-  const sub = createSolanaRpcSubscriptions({ transport: ssp })
-  const sender = createDefaultTransactionSender({
-    rpc: api,
-    rpcSubscriptions: sub
-  })
-  
+
+  async function kp(): Promise<CryptoKeyPair> {
+    if (process.env.KP != null) {
+      let kpbytes = await fs.readFile(process.env.KP)
+      let js = JSON.parse(kpbytes.toString())
+      let bytes = new Uint8Array(js)
+      return createKeyPairFromBytes(bytes)
+    }
+    return generateKeyPair();
+  }
 
   beforeEach(async () => {
     const depl = await deploymentAddress(SIMPLE_IMAGE_ID);
@@ -39,17 +43,11 @@ describe('BonsolProgram', () => {
     const deployAccount = await api.getAccountInfo(depl, { commitment: "confirmed", encoding: "base64" }).send();
     if (!deployAccount.value) {
       //deploy
-      const keyPair = await generateKeyPair();
+      const keyPair = await kp();
       const pub = await getAddressFromPublicKey(keyPair.publicKey);
-      const airdrop = createDefaultAirdropRequester({
-        rpc: api,
-        rpcSubscriptions: sub
-      })
-      await airdrop({
-        commitment: "confirmed",
-        lamports: lamports(1000000000n),
-        recipientAddress: pub
-      })
+      if (process.env.RPC_ENDPOINT == null || process.env.RPC_ENDPOINT == "http://localhost:8899") {
+        await api.requestAirdrop(pub, lamports(1000000000n)).send();
+      }
       const result = await Deploy(
         {
           imageUrl: "https://elfserver.solana",
@@ -61,45 +59,44 @@ describe('BonsolProgram', () => {
             ProgramInputType.Private
           ]
         })
-     
+
       const ctx = await api.getLatestBlockhash().send()
-      const x = pipe(
-        createTransaction({ version: 0 }),
-        tx => setTransactionFeePayer(pub, tx),
-        tx => setTransactionLifetimeUsingBlockhash(ctx.value, tx),
-        tx => appendTransactionInstruction(result, tx),
-        async tx => signTransaction([keyPair], tx),
+      const txn = pipe(
+        createTransactionMessage({ version: 0 }),
+        tx => appendTransactionMessageInstruction(result, tx),
+        tx => setTransactionMessageLifetimeUsingBlockhash(ctx.value, tx),
+        tx => setTransactionMessageFeePayer(pub, tx),
+        tx => compileTransaction(tx),
+      );
+      const signed = await signTransaction([keyPair], txn)
+      const send = pipe(
+        getBase64EncodedWireTransaction(signed),
         async tx => {
-          await sender(await tx, { commitment: "confirmed", skipPreflight: true });
+          await api.sendTransaction(await tx, { skipPreflight: false }).send()
           return tx;
         },
-        async tx => api.getTransaction(getSignatureFromTransaction(await tx), {
+      )
+      await pipe(
+        api.getTransaction(getSignatureFromTransaction(signed), {
           commitment: "confirmed",
           maxSupportedTransactionVersion: 0
         }).send(),
         async tx => {
           console.log((await tx)?.meta?.logMessages)
           return tx;
-        }
-      );
-      await x;
+        })
+
     }
   })
 
   it('should create valid execution requests', async () => {
-    const keyPair = await generateKeyPair();
+    const keyPair = await kp()
 
 
     const pub = await getAddressFromPublicKey(keyPair.publicKey);
-    const airdrop = createDefaultAirdropRequester({
-      rpc: api,
-      rpcSubscriptions: sub
-    })
-    await airdrop({
-      commitment: "confirmed",
-      lamports: lamports(1000000000n),
-      recipientAddress: pub
-    })
+    if (process.env.RPC_ENDPOINT == null || process.env.RPC_ENDPOINT == "http://localhost:8899") {
+      await api.requestAirdrop(pub, lamports(1000000000n)).send();
+    }
     //random uuid 
     const eid = randomBytes(5).toString();
     const input = JSON.stringify({ "attestation": "test" })
@@ -122,20 +119,28 @@ describe('BonsolProgram', () => {
         requester: pub
       }
     )
-   
+
     const ctx = await api.getLatestBlockhash().send()
-    expect(async () => {
-      const x = pipe(
-        createTransaction({ version: 0 }),
-        tx => setTransactionFeePayer(pub, tx),
-        tx => setTransactionLifetimeUsingBlockhash(ctx.value, tx),
-        tx => appendTransactionInstruction(result, tx),
-        async tx => signTransaction([keyPair], tx),
+    await expect(async () => {
+      const txn = pipe(
+        createTransactionMessage({ version: 0 }),
+        tx => setTransactionMessageFeePayer(pub, tx),
+        tx => setTransactionMessageLifetimeUsingBlockhash(ctx.value, tx),
+        tx => appendTransactionMessageInstruction(result, tx),
+        tx => compileTransaction(tx),
+      );
+
+      const signed = await signTransaction([keyPair], txn)
+      const send = pipe(
+        getBase64EncodedWireTransaction(signed),
         async tx => {
-          await sender(await tx, { commitment: "confirmed"});
+          await api.sendTransaction(await tx, { skipPreflight: false }).send();
           return tx;
         },
-        async tx => api.getTransaction(getSignatureFromTransaction(await tx), {
+
+      );
+      await pipe(
+        api.getTransaction(getSignatureFromTransaction(signed), {
           commitment: "confirmed",
           maxSupportedTransactionVersion: 0
         }).send(),
@@ -143,9 +148,8 @@ describe('BonsolProgram', () => {
           console.log((await tx)?.meta?.logMessages)
           return tx;
         }
-      );
-      await x;
+      )
     }).not.toThrow()
-
+    await new Promise((resolve) => setTimeout(resolve, 1000))
   })
 })
