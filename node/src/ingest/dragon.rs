2bbb14dec4715a1f388f0969cf1461ad4855a723
eureka-cpu <github.eureka@gmail.com>
2024-10-16 15:22:30 -0700

    abstract dragon ingester functions

--- node/src/ingest/dragon.rs
+++ node/src/ingest/dragon.rs
@@ -1,5 +1,12 @@
 use std::{collections::HashMap, time::Duration};
 
+use {
+    anyhow::anyhow,
+    solana_sdk::{message::AccountKeys, transaction::VersionedTransaction},
+    solana_transaction_status::TransactionStatusMeta,
+    yellowstone_grpc_proto::geyser::SubscribeUpdate,
+};
+
 use crate::types::BonsolInstruction;
 
 use {
@@ -42,7 +49,28 @@ impl GrpcIngester {
     }
 }
 
-async fn spawn_op(
+impl Ingester for GrpcIngester {
+    fn start(&mut self, program: Pubkey) -> Result<TxChannel> {
+        let (txchan, rx) = tokio::sync::mpsc::unbounded_channel();
+        let stream_client = GeyserGrpcClient::build_from_shared(self.url.clone())?
+            .x_token(Some(self.token.clone()))?
+            .connect_timeout(Duration::from_secs(
+                self.connection_timeout_secs.unwrap_or(10) as u64,
+            ))
+            .timeout(Duration::from_secs(self.timeout_secs.unwrap_or(10) as u64));
+        self.op_handle = Some(tokio::spawn(async move {
+            ingest(program, txchan, stream_client).await
+        }));
+        Ok(rx)
+    }
+
+    fn stop(&mut self) -> Result<()> {
+        self.op_handle.as_mut().map(|t| t.abort());
+        Ok(())
+    }
+}
+
+async fn ingest(
     program: Pubkey,
     txchan: UnboundedSender<Vec<BonsolInstruction>>,
     stream_client: GeyserGrpcBuilder,
@@ -68,117 +96,115 @@ async fn spawn_op(
     while let Some(message) = stream.next().await {
         match message {
             Ok(msg) => {
-                if let Some(UpdateOneof::Transaction(txw)) = msg.update_oneof {
-                    if let Some(tx) = txw.transaction {
-                        if let Ok(soltxn) = create_tx_with_meta(tx) {
-                            let acc = soltxn.account_keys();
-                            let txndata = soltxn.get_transaction();
-                            let meta = soltxn.get_status_meta();
-                            //unwrap so we can consume
-                            if let VersionedMessage::V0(msg) = txndata.message {
-                                let bonsolixs = msg
-                                    .instructions
-                                    .into_iter()
-                                    .filter(|ix| {
-                                        acc.get(ix.program_id_index as usize) == Some(&program)
-                                    })
-                                    .map(|ix| BonsolInstruction {
-                                        cpi: false,
-                                        accounts: ix
-                                            .accounts
-                                            .into_iter()
-                                            .map(|idx| {
-                                                acc.get(idx as usize)
-                                                    .map(|a| *a)
-                                                    .unwrap_or_default()
-                                            })
-                                            .collect(),
-                                        data: ix.data,
-                                        last_known_block: txw.slot,
-                                    })
-                                    .collect::<Vec<BonsolInstruction>>();
-                                if bonsolixs.len() > 0 {
-                                    if let Err(e) = txchan.send(bonsolixs) {
-                                        eprintln!("Error sending to txn ingest channel: {e:?}")
-                                    }
-                                }
-
-                                if let Some(metadata) = meta {
-                                    if let Some(inner_ix) = metadata.inner_instructions {
-                                        let ixs = inner_ix
-                                            .into_iter()
-                                            .flat_map(|ix| {
-                                                ix.instructions
-                                                    .into_iter()
-                                                    .filter(|ix| {
-                                                        acc.get(
-                                                            ix.instruction.program_id_index
-                                                                as usize,
-                                                        ) == Some(&program)
-                                                    })
-                                                    .map(|ix| BonsolInstruction {
-                                                        cpi: true,
-                                                        accounts: ix
-                                                            .instruction
-                                                            .accounts
-                                                            .into_iter()
-                                                            .map(|a| {
-                                                                acc.get(a as usize)
-                                                                    .map(|a| *a)
-                                                                    .unwrap_or_default()
-                                                            })
-                                                            .collect(),
-                                                        data: ix.instruction.data,
-                                                        last_known_block: txw.slot,
-                                                    })
-                                                    .collect::<Vec<BonsolInstruction>>()
-                                            })
-                                            .collect::<Vec<BonsolInstruction>>();
-                                        if ixs.len() > 0 {
-                                            match txchan.send(ixs) {
-                                                Ok(_) => {}
-                                                Err(e) => {
-                                                    println!(
-                                                        "Error sending to txn ingest channel: {:?}",
-                                                        e
-                                                    );
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+                if let Err(e) = handle_msg(msg, program, &txchan) {
+                    eprintln!("Error in stream: {e:?}")
                 }
             }
-            Err(_) => {
-                eprintln!("Error in stream");
-            }
+            Err(e) => eprintln!("Error in stream: {e:?}"),
         }
     }
     Ok(())
 }
 
-impl Ingester for GrpcIngester {
-    fn start(&mut self, program: Pubkey) -> Result<TxChannel> {
-        let (txchan, rx) = tokio::sync::mpsc::unbounded_channel();
-        let stream_client = GeyserGrpcClient::build_from_shared(self.url.clone())?
-            .x_token(Some(self.token.clone()))?
-            .connect_timeout(Duration::from_secs(
-                self.connection_timeout_secs.unwrap_or(10) as u64,
-            ))
-            .timeout(Duration::from_secs(self.timeout_secs.unwrap_or(10) as u64));
-        self.op_handle = Some(tokio::spawn(async move {
-            spawn_op(program, txchan, stream_client).await
-        }));
-        Ok(rx)
+fn handle_msg(
+    msg: SubscribeUpdate,
+    program: Pubkey,
+    txchan: &UnboundedSender<Vec<BonsolInstruction>>,
+) -> Result<()> {
+    if let Some(UpdateOneof::Transaction(txw)) = msg.update_oneof {
+        txw.transaction.map(|tx| -> Result<()> {
+            create_tx_with_meta(tx)
+                .map(|soltxn| {
+                    try_send_instructions(
+                        program,
+                        txw.slot,
+                        soltxn.account_keys(),
+                        soltxn.get_transaction(),
+                        soltxn.get_status_meta(),
+                        &txchan,
+                    )
+                })
+                .map_err(|e| anyhow!("error while sending instructions: {e}"))?
+        });
     }
+    Ok(())
+}
 
-    fn stop(&mut self) -> Result<()> {
-        self.op_handle.as_mut().map(|t| t.abort());
-        Ok(())
+fn try_send_instructions(
+    program: Pubkey,
+    last_known_block: u64,
+    acc: AccountKeys,
+    txndata: VersionedTransaction,
+    meta: Option<TransactionStatusMeta>,
+    txchan: &UnboundedSender<Vec<BonsolInstruction>>,
+) -> Result<()> {
+    if let VersionedMessage::V0(msg) = txndata.message {
+        let bonsolixs = msg
+            .instructions
+            .into_iter()
+            .filter_map(|ix| {
+                if acc
+                    .get(ix.program_id_index as usize)
+                    .is_some_and(|p| p == &program)
+                {
+                    return Some(BonsolInstruction::new(
+                        false,
+                        ix.accounts
+                            .into_iter()
+                            .filter_map(|idx| acc.get(idx as usize).copied())
+                            .collect(),
+                        ix.data,
+                        last_known_block,
+                    ));
+                }
+                None
+            })
+            .collect::<Vec<BonsolInstruction>>();
+        if !bonsolixs.is_empty() {
+            txchan.send(bonsolixs).map_err(|e| {
+                anyhow!(
+                    "failed to send instructions to txn ingest channel: {:?}",
+                    e.0
+                )
+            })?
+        }
+        if let Some(metadata) = meta {
+            if let Some(inner_ix) = metadata.inner_instructions {
+                let ixs = inner_ix
+                    .into_iter()
+                    .flat_map(|ix| {
+                        ix.instructions.into_iter().filter_map(|ix| {
+                            if acc
+                                .get(ix.instruction.program_id_index as usize)
+                                .is_some_and(|p| p == &program)
+                            {
+                                return Some(BonsolInstruction::new(
+                                    true,
+                                    ix.instruction
+                                        .accounts
+                                        .into_iter()
+                                        .filter_map(|a| acc.get(a as usize).copied())
+                                        .collect(),
+                                    ix.instruction.data,
+                                    last_known_block,
+                                ));
+                            }
+                            None
+                        })
+                    })
+                    .collect::<Vec<BonsolInstruction>>();
+                if !ixs.is_empty() {
+                    txchan.send(ixs).map_err(|e| {
+                        anyhow!(
+                            "failed to send instructions to txn ingest channel: {:?}",
+                            e.0
+                        )
+                    })?
+                }
+            }
+        }
     }
+    Ok(())
 }
 
 #[cfg(test)]
--- node/src/types.rs
+++ node/src/types.rs
@@ -8,6 +8,17 @@ pub struct BonsolInstruction {
     pub last_known_block: u64,
 }
 
+impl BonsolInstruction {
+    pub fn new(cpi: bool, accounts: Vec<Pubkey>, data: Vec<u8>, last_known_block: u64) -> Self {
+        Self {
+            cpi,
+            accounts,
+            data,
+            last_known_block,
+        }
+    }
+}
+
 pub enum CallbackStatus {
     Completed,
     Failure,
